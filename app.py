025d47d16487860586ec8bf0524b10253db11029
SOBANEJAZ <sobanpythonista@gmail.com>
2024-08-04 07:27:43 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Social Media Checker")
+st.write("Mail: https://mail.google.com/mail/u/0/#inbox")
+st.write("Mail: https://mail.google.com/mail/u/1/#inbox")
+st.write("Instagram: www.instagram.com")
+st.write("Linkedin -https://www.linkedin.com/feed/")
+st.write("YouTube -https://www.youtube.com/feed/subscriptions")
+st.write("Reddit -https://www.reddit.com/?feed=home")
