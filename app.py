a34260644f8d3fd7059ac3c6e1c58c973376944d
Soban Ejaz <sobanpythonista@gmail.com>
2024-09-19 08:21:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 import streamlit as st
 
 st.title("Social Media Checker")
-st.write("Mail: https://mail.google.com/mail/u/0/#inbox")
-st.write("Mail: https://mail.google.com/mail/u/1/#inbox")
+st.title("Always open")
+st.write("Mail 1: https://mail.google.com/mail/u/0/#inbox")
+st.write("Mail 2: https://mail.google.com/mail/u/1/#inbox")
+st.write("Upwork: https://www.upwork.com")
+st.title("no important")
 st.write("YouTube -https://www.youtube.com/feed/subscriptions")
 st.write("Github -https://github.com/")
+st.write("Github -https://instagram.com/")
+st.write("Github -https://goodreads.com/")
+st.write("Github -https://linkedin.com/")
+
+st.title("Then")
+st.write("1 reasearch video, 2 adobe videos, and then coding videos on Youtube")
